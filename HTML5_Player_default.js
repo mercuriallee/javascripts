2b18e78ca83cd63c53c1bcf2f27f9e9dd41d630a
Mercurial Lee <jimblue@126.com>
2015-04-05 17:17:12 +0800

    add HTML5_Player_default.js

---
+++ HTML5_Player_default.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         HTML5 player default
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  enter something useful
+// @author       You
+// @match        http://v.youku.com/v_show/*
+// @require      http://code.jquery.com/jquery-latest.js
+// @grant        none
+// ==/UserScript==
+
+$('#ADT-PlayHTML5-btn').click();
